36037ccb64719967e24207ab987938a10651fbfb
unknown <joshxchris098@gmail.com>
2025-07-25 20:07:46 +0100

    final again

--- investment-deal/routes/index.js
+++ investment-deal/routes/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch'); // npm install node-fetch
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'EquiTrust' });
+  res.render('home', { title: 'Capixion' });
 });
 
 router.get('/api/crypto', async (req, res) => {
@@ -19,47 +19,47 @@ router.get('/api/crypto', async (req, res) => {
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Sign Up | EquiTrust' });
+  res.render('signup', { title: 'Sign Up | Capixion' });
 });
 
 router.get('/signin', function(req, res, next) {
-  res.render('signin', { title: 'Sign In | EquiTrust' });
+  res.render('signin', { title: 'Sign In | Capixion' });
 });
 
 router.get('/dashboard', (req, res) => {
   res.render('dashboard', { 
-    title: 'EquiTrust - Dashboard',
+    title: 'Capixion - Dashboard',
     page: 'dashboard' 
   });
 });
 
 router.get('/investment-plans', function(req, res, next) {
-  res.render('investment-plans', { title: 'Investment Plans | EquiTrust' });
+  res.render('investment-plans', { title: 'Investment Plans | Capixion' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About | EquiTrust' });
+  res.render('about', { title: 'About | Capixion' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact | EquiTrust' });
+  res.render('contact', { title: 'Contact | Capixion' });
 });
 
 router.get('/blog', function(req, res, next) {
-  res.render('blog', { title: 'Blog | EquiTrust' });
+  res.render('blog', { title: 'Blog | Capixion' });
 });
 router.get('/deposit', function(req, res, next) {
   res.render('deposit', { title: 'Deposit | EquiTrust' });
 });
-router.get('/transaction-log', function(req, res, next) {
-  res.render('transaction', { title: 'Transaction | EquiTrust' });
+router.get('/transaction', function(req, res, next) {
+  res.render('transaction', { title: 'Transaction | Capixion' });
 });
 
 router.get('/withdraw', function(req, res, next) {
-  res.render('withdraw', { title: 'Withdraw | EquiTrust' });
+  res.render('withdraw', { title: 'Withdraw | Capixion' });
 });
 router.get('/investment', function(req, res, next) {
-  res.render('investment', { title: 'Investment | EquiTrust' });
+  res.render('investment', { title: 'Investment | Capixion' });
 });
 
 module.exports = router;
